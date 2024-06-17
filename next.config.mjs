ceb6ee3eaf72dad411046a614d97fccb7337fe62
Madusara Liyanage <40194355+madusara@users.noreply.github.com>
2024-06-17 13:05:52 +0530

    Add cors configurations to next app

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  // Your Next.js config here
   async headers() {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
